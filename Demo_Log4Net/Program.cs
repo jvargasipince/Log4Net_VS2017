96f8dac828a291e55c5313b089523af3c3f3bdaf
jvargasipince <jvargas.ipince@gmail.com>
2018-01-15 14:18:59 -0500

    Agregar archivos de proyecto.

---
+++ Demo_Log4Net/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Demo_Log4Net
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void Main(string[] args)
+        {
+
+            log.Info("Bienvenidos a nuestra demo de Log4Net");
+            Console.WriteLine("Presione enter");
+            Console.ReadLine();
+        }
+    }
+}
